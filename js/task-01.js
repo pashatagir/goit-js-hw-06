8b9cda780ae9891f9a0523a1589433a25792a528
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-09 13:05:54 +0200

    finish task1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const allCategoriesEl = document.querySelectorAll(".item");
+console.log("Number of categories:", allCategoriesEl.length);
+const allHeadingEl = document.querySelectorAll("h2");
+const listInCategoryEl = document.querySelectorAll(".item ul");
+console.log("Category:", allHeadingEl[0].textContent);
+console.log("Elements:", listInCategoryEl[0].querySelectorAll("li").length);
+console.log("Category:", allHeadingEl[1].textContent);
+console.log("Elements:", listInCategoryEl[1].querySelectorAll("li").length);
+console.log("Category:", allHeadingEl[2].textContent);
+console.log("Elements:", listInCategoryEl[2].querySelectorAll("li").length);
