a5a7f6436be156b0cd827723382b26e0ab7de1ad
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-12 16:00:26 +0200

    task 3 fixed

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,7 @@
 const allCategoriesEl = document.querySelectorAll(".item");
-
-const allHeadingEl = document.querySelectorAll("h2");
-
-const listInCategoryEl = document.querySelectorAll(".item ul");
-
 console.log("Number of categories:", allCategoriesEl.length);
-
-console.log("Category:", allHeadingEl[0].textContent);
-console.log("Elements:", listInCategoryEl[0].querySelectorAll("li").length);
-
-console.log("Category:", allHeadingEl[1].textContent);
-console.log("Elements:", listInCategoryEl[1].querySelectorAll("li").length);
-
-console.log("Category:", allHeadingEl[2].textContent);
-console.log("Elements:", listInCategoryEl[2].querySelectorAll("li").length);
+const allTitlesEl = document.querySelectorAll("h2");
+allTitlesEl.forEach((title) => {
+  console.log("Category:", title.textContent);
+  console.log("Elements:", title.nextElementSibling.children.length);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const makeOneItemMarkup = (ingredients) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredients;
+  return itemEl;
+};
+
+const makeAllItemsMarkup = ingredients.map(makeOneItemMarkup);
+
+const listEl = document.querySelector("#ingredients");
+
+listEl.append(...makeAllItemsMarkup);
