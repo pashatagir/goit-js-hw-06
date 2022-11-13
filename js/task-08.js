d015ab0990c67a99160d73ca9abef07a6ce857d3
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-13 21:39:56 +0200

    fixed task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+  inputEmail: document.querySelector("[type=email]"),
+  inputPassword: document.querySelector("[type=password]"),
+  btn: document.querySelector("[type=submit]"),
+};
 
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const dataForm = {
+    [refs.inputEmail.name]: refs.inputEmail.value,
+    [refs.inputPassword.name]: refs.inputPassword.value,
+  };
+  if (refs.inputEmail.value && refs.inputPassword.value !== "") {
+    console.log(dataForm);
+    refs.form.reset();
+  } else {
+    alert("Всі поля повинні бути заповнені!");
+  }
+}
