d1392545063f08b403f3ed7678a2f8ab9975101f
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-13 22:55:57 +0200

    fixed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  btn: document.querySelector(".change-color"),
+  output: document.querySelector(".color"),
+};
+
+refs.btn.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  refs.output.textContent = getRandomHexColor();
+  refs.body.style.backgroundColor = refs.output.textContent;
+}
